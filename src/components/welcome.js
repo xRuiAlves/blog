7124ed4d6c51760407a2f1484274bbc5160f01f1
Rui Alves <up201606746@fe.up.pt>
2020-07-29 23:08:53 +0100

    Improve mobile home page layout

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 const Welcome = () => (
-        <>
-            <h2>Welcome!</h2>
-            <p>
-                My name is Rui Alves and I'm a Computer Engineering MSc student @ FEUP and a tech enthusiast.
-            </p>
-            <p>
-                Here you may find articles of tech-related stuff I found particular interest in doing.
-            </p>
-        </>
+    <>
+        <h2>Welcome!</h2>
+        <p>
+            My name is Rui Alves and I'm a Computer Engineering MSc student @ FEUP and a tech enthusiast.
+        </p>
+        <p>
+            Here you may find articles of tech-related stuff I found particular interest in doing.
+        </p>
+        <p>
+            If you want to stay tuned, this blog is available as an <a className="emphasized-anchor" href="rss.xml">RSS feed</a>.
+        </p>
+    </>
 );
 
 export default Welcome;
