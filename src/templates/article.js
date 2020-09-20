86fc5bb8ba59071d57bb2b0e64f784ec5907a3f5
Rui Alves <up201606746@fe.up.pt>
2020-09-20 21:59:44 +0100

    Add article title to page title

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 import "../css/article.css";
 
@@ -12,6 +13,7 @@ export default function Article({
 
     return (
         <Layout>
+            <SEO title={frontmatter.title} />
             <div className="article-container">
                 <article className="article">
                     <header>
