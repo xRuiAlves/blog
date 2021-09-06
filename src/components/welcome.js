4aec337ec0711d01309696df5ace6e975b7d9b73
xRuiAlves <ruialves.esrt.98@gmail.com>
2021-09-06 19:48:52 +0100

    Update role

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,7 +4,7 @@ const Welcome = () => (
     <>
         <h2>Welcome!</h2>
         <p>
-            My name is Rui Alves and I'm a Computer Engineering MSc student @ FEUP and a tech enthusiast.
+            My name is Rui Alves and I'm a Software Engineer and tech enthusiast.
         </p>
         <p>
             Here you may find articles of tech-related stuff I found particular interest in doing.
