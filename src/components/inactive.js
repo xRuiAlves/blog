362907b4fd792202bbb99db1985692dc56928e7c
xRuiAlves <ruialves.esrt.98@gmail.com>
2022-03-24 16:59:58 +0000

    Add inactivity notice

---
+++ src/components/inactive.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../css/common.css";
+import "../css/inactive.css";
+
+const Inactive = () => (
+    <>
+        <p className="inactive">
+            This blog is currently <strong>inactive</strong>. I haven't felt a lot of motivation lately to write things up, but hey, I may return sometime in the future! 🚀 
+        </p>
+    </>
+);
+
+export default Inactive;
